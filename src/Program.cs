068e95a527d63fe25c2af5a8cba997e4076d873a
Ben Swabey <ben.swabey@teachertracker.co.uk>
2024-05-16 22:52:42 +0100

    Creation of basic brute-force collatz verification

---
+++ src/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+
+namespace collatzcracked;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        long iterations = 1000000000;
+    
+        int numCores = Environment.ProcessorCount;
+
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+
+        // Outer loop to loop through the iterations
+        Parallel.For(1, iterations + 1, new ParallelOptions { MaxDegreeOfParallelism = numCores }, (i) =>
+        {
+            long startingNumber = i;
+            long currentNumber = i;
+            long stepsCounter = 0;
+
+            // Inner loop to complete the collatz conjecture
+            while (currentNumber != 1)
+            {
+                stepsCounter++;
+
+                if (currentNumber % 2 == 0)
+                    currentNumber = currentNumber / 2;
+                else
+                    currentNumber = currentNumber * 3 + 1;
+            }
+        });
+
+        watch.Stop();
+
+        Console.WriteLine($"All numbers up to {iterations} resolve down to 1!");
+        Console.WriteLine($"Time taken: {watch.ElapsedMilliseconds}");
+    }
+}
